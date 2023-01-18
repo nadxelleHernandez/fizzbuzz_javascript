63c2d62251c199269ff001742227bd14b99d204c
nadxelle <nadxellehernandez@gmail.com>
2023-01-18 12:02:29 -0800

    added html

---
+++ src/index.js
@@ -0,0 +1,20 @@
+console.log('Hello world!');
+const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+console.log(getRandomNumber(10));
+console.log(getRandomNumber(100));
+
+const getCurrentTime = () => {
+  const currentDate = new Date();
+  return (
+    currentDate.getHours() +
+    ':' +
+    currentDate.getMinutes() +
+    ':' +
+    currentDate.getSeconds()
+  );
+};
+
+console.log(`The current time is ${getCurrentTime()}.`);
